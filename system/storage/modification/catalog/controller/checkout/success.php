e7e402b0c627c87b9febca2615416935f4323df9
aakritikishore <aakritikishore@codaemonsoftwares.com>
2019-10-22 12:32:45 +0530

    change file

--- system/storage/modification/catalog/controller/checkout/success.php
+++ system/storage/modification/catalog/controller/checkout/success.php
@@ -4,29 +4,33 @@ class ControllerCheckoutSuccess extends Controller {
 		$this->load->language('checkout/success');
 
 		if (isset($this->session->data['order_id'])) {
+
+			//generate vend receipt
+			$this->generate_vend_receipt($this->session->data['order_id']);
+
 			$this->cart->clear();
 
 			// Add to activity log
-			if ($this->config->get('config_customer_activity')) {
-				$this->load->model('account/activity');
+		if ($this->config->get('config_customer_activity')) {
+			$this->load->model('account/activity');
 
-				if ($this->customer->isLogged()) {
-					$activity_data = array(
-						'customer_id' => $this->customer->getId(),
-						'name'        => $this->customer->getFirstName() . ' ' . $this->customer->getLastName(),
-						'order_id'    => $this->session->data['order_id']
-					);
+			if ($this->customer->isLogged()) {
+				$activity_data = array(
+					'customer_id' => $this->customer->getId(),
+					'name'        => $this->customer->getFirstName() . ' ' . $this->customer->getLastName(),
+					'order_id'    => $this->session->data['order_id']
+				);
 
-					$this->model_account_activity->addActivity('order_account', $activity_data);
-				} else {
-					$activity_data = array(
-						'name'     => $this->session->data['guest']['firstname'] . ' ' . $this->session->data['guest']['lastname'],
-						'order_id' => $this->session->data['order_id']
-					);
+				$this->model_account_activity->addActivity('order_account', $activity_data);
+			} else {
+				$activity_data = array(
+					'name'     => $this->session->data['guest']['firstname'] . ' ' . $this->session->data['guest']['lastname'],
+					'order_id' => $this->session->data['order_id']
+				);
 
-					$this->model_account_activity->addActivity('order_guest', $activity_data);
-				}
+				$this->model_account_activity->addActivity('order_guest', $activity_data);
 			}
+                }       
 
 			unset($this->session->data['shipping_method']);
 			unset($this->session->data['shipping_methods']);
@@ -39,11 +43,14 @@ class ControllerCheckoutSuccess extends Controller {
 			unset($this->session->data['reward']);
 			unset($this->session->data['voucher']);
 			unset($this->session->data['vouchers']);
+                        unset($this->session->data['credits']);
 
             unset($this->session->data['credits']);
             
 			unset($this->session->data['totals']);
-		}
+                        unset($this->session->data['speedy_delivery']);
+                        unset($this->session->data['distance_delivery_fee']);
+                    }
 
 		$this->document->setTitle($this->language->get('heading_title'));
 
@@ -61,7 +68,7 @@ class ControllerCheckoutSuccess extends Controller {
 
 		$data['breadcrumbs'][] = array(
 			'text' => $this->language->get('text_checkout'),
-			'href' => $this->url->link('checkout/checkout', '', true)
+			'href' => $this->url->link('checkout/checkout', '', 'SSL')
 		);
 
 		$data['breadcrumbs'][] = array(
@@ -72,7 +79,7 @@ class ControllerCheckoutSuccess extends Controller {
 		$data['heading_title'] = $this->language->get('heading_title');
 
 		if ($this->customer->isLogged()) {
-			$data['text_message'] = sprintf($this->language->get('text_customer'), $this->url->link('account/account', '', true), $this->url->link('account/order', '', true), $this->url->link('account/download', '', true), $this->url->link('information/contact'));
+			$data['text_message'] = sprintf($this->language->get('text_customer'), $this->url->link('account/account', '', 'SSL'), $this->url->link('account/order', '', 'SSL'), $this->url->link('account/download', '', 'SSL'), $this->url->link('information/contact'));
 		} else {
 			$data['text_message'] = sprintf($this->language->get('text_guest'), $this->url->link('information/contact'));
 		}
@@ -89,5 +96,107 @@ class ControllerCheckoutSuccess extends Controller {
 		$data['header'] = $this->load->controller('common/header');
 
 		$this->response->setOutput($this->load->view('common/success', $data));
+		
+	}
+
+	public function generate_vend_receipt($order_id=0) 
+	{
+		//set the vend POS library
+		$vend = $this->registry->set('vendpos', new Vendpos ($this->registry));
+
+		if($order_id)
+		{
+			//get order related data
+			$this->load->model('account/vend');
+			$order_data 	= $this->model_account_vend->getOrderDetailsForVend($order_id);
+			if($order_data)
+			{
+				//insert status as pending
+				//prepare sales array
+				$vend_sale_array	= array(
+					'order_id'	=> $order_id,
+					'status'	=> 'Pending'
+				);
+
+				$oc_vend_id	= $this->model_account_vend->insert_vend_sale_into_opencart($vend_sale_array);
+				
+				if($oc_vend_id)
+				{
+					//call to vend POS API
+					//$sale_id 	= $this->vendpos->vend_automation($order_data);	
+                                        $vend_data 	= $this->vendpos->vend_automation($order_data);
+                                        $sale_id = $vend_data['sale_id'];
+                                        $status = $vend_data['vend_order_status'];
+
+					//prepare sales array
+					$vend_sale_array	= array(
+						'id'			=> $oc_vend_id,
+						'order_id'		=> $order_id,
+						'vend_sale_id'	=> $sale_id,
+						//'status'		=> 'Processed'
+					);
+
+					if($sale_id && $status=='ONACCOUNT')
+					{
+                                            //set status as SAVED for UGO Credit
+                                            $vend_sale_array['status']	= 'Processed';
+                                        }else if($sale_id && $status == 'CLOSED') {
+                                            //set status as processed
+                                            $vend_sale_array['status']	= 'Processed';
+					}
+					else
+					{
+						//set status as Unprocessed
+						$vend_sale_array['status']	= 'Unprocessed';
+					}
+                                        
+                                        $vend_sale_array['vend_status']	= $status;
+
+					//update in database
+					$oc_vend_status	= $this->model_account_vend->update_vend_sale_into_opencart($vend_sale_array);
+
+					if($oc_vend_status)
+					{
+						//set log contents
+						$log_contents	= 'Updated status for order id: '.$order_id.' in opencart.';
+
+						//write to log
+						$this->vendpos->log_content($log_contents);
+					}
+					else
+					{
+						//set log contents
+						$log_contents	= 'Problem in updating the status for order id: '.$order_id.' in opencart.';
+
+						//write to log if fails
+						$this->vendpos->log_content($log_contents);
+					}
+				}
+				else
+				{
+					//set log contents
+					$log_contents	= 'Not able to insert order id in vend sales summary table.';
+
+					//write to log if fails
+					$this->vendpos->log_content($log_contents);
+				}
+			}
+			else
+			{
+				//set log contents
+				$log_contents	= 'Failed on success page because the order data not found.';
+
+				//write to log if fails
+				$this->vendpos->log_content($log_contents);
+			}
+		}
+		else
+		{
+			//set log contents
+			$log_contents	= 'Order Id not provided for making entry in Vend POS';
+
+			//write to log if fails
+			$this->vendpos->log_content($log_contents);
+		}
 	}
 }
