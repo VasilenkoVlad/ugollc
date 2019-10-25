53247642525f4d011d81834654a4b8f2bd5ea409
Ubuntu <ubuntu@ip-172-31-22-254.ec2.internal>
2019-10-25 10:11:02 +0000

    test master branch

--- config.php
+++ config.php
@@ -1,28 +1,28 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/ugollc/');
+define('HTTP_SERVER', 'http://35.175.254.127/ugollc/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/ugollc/');
+define('HTTPS_SERVER', 'http://35.175.254.127/ugollc/');
 
 // DIR
-define('DIR_APPLICATION', '/opt/lampp/htdocs/ugollc/catalog/');
-define('DIR_SYSTEM', '/opt/lampp/htdocs/ugollc/system/');
-define('DIR_IMAGE', '/opt/lampp/htdocs/ugollc/image/');
-define('DIR_LANGUAGE', '/opt/lampp/htdocs/ugollc/catalog/language/');
-define('DIR_TEMPLATE', '/opt/lampp/htdocs/ugollc/catalog/view/theme/');
-define('DIR_CONFIG', '/opt/lampp/htdocs/ugollc/system/config/');
-define('DIR_CACHE', '/opt/lampp/htdocs/ugollc/system/storage/cache/');
-define('DIR_DOWNLOAD', '/opt/lampp/htdocs/ugollc/system/storage/download/');
-define('DIR_LOGS', '/opt/lampp/htdocs/ugollc/system/storage/logs/');
-define('DIR_MODIFICATION', '/opt/lampp/htdocs/ugollc/system/storage/modification/');
-define('DIR_UPLOAD', '/opt/lampp/htdocs/ugollc/system/storage/upload/');
+define('DIR_APPLICATION', '/var/www/html/ugollc/catalog/');
+define('DIR_SYSTEM', '/var/www/html/ugollc/system/');
+define('DIR_IMAGE', '/var/www/html/ugollc/image/');
+define('DIR_LANGUAGE', '/var/www/html/ugollc/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/ugollc/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/ugollc/system/config/');
+define('DIR_CACHE', '/var/www/html/ugollc/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/ugollc/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/ugollc/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/ugollc/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/ugollc/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'newugollc');
+define('DB_USERNAME', 'codaemon');
+define('DB_PASSWORD', 'ugollc@codaemon');
+define('DB_DATABASE', 'ugollc');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
